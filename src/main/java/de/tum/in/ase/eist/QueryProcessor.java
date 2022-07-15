1f405de5d078d2071945a0b90bc51fe09b4660cd
ge95hir <ge95hir@mytum.de>
2022-07-15 13:21:52 +0200

    Hi

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -18,13 +18,14 @@ public class QueryProcessor {
             return "Stephanie";
         } else if (query.contains("which of the following numbers is the largest")) { // TODO extend the programm here
             String s = query.substring(57);
-            String[] stri = s.split(",");
+            String[] stri = s.split(", ");
+            int length = stri.length;
             int[] in = new int[stri.length];
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < length; i++) {
                 in[i] = Integer.parseInt(stri[i]);
             }
             int f = in[0];
-            for (int i = 1; i < 4; i++) {
+            for (int i = 1; i < length; i++) {
                 if (in[i] > f) {
                     f = in[i];
                 }
@@ -32,5 +33,10 @@ public class QueryProcessor {
             return f + "";
         }
         return "";
+    }
+
+    public static void main(String[] args) {
+        QueryProcessor q = new QueryProcessor();
+        q.process("4b35d380: which of the following numbers is the largest: 47, 921, 98, 29");
     }
 }
