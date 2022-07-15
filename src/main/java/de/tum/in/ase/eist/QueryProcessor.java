48d07018e2721a82ec5fb98332fbaa9091e4559e
ge95hir <ge95hir@mytum.de>
2022-07-15 13:06:34 +0200

    Hi

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,9 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QueryProcessor {
 
@@ -12,9 +15,23 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Stephanie";
-        } else { // TODO extend the programm here
-            return "";
+            return "Stephanie";
+        } else if (query.contains("which of the following numbers is the largest")) { // TODO extend the programm here
+            String[] l;
+            String s = query.substring(57);
+            l = s.split(",");
+            int[] in = new int[4];
+            for (int i = 0; i < 4; i++) {
+                in[i] = Integer.parseInt(l[i]);
+            }
+            int f = in[0];
+            for (int i = 1; i < 4; i++) {
+                if (in[i] > f) {
+                    f = in[i];
+                }
+            }
+            return f + "";
         }
+        return "";
     }
 }
